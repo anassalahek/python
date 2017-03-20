90a370b56357655d3718209d19e2700206d3d5d5
anassalahek <anassalahek@gmail.com>
2017-03-20 21:45:46 +0700

    Update form.py

--- form.py
+++ form.py
@@ -9,4 +9,8 @@ url1 = 'http://yourserver/checkout1.php'
 url2 = 'http://yourserver/checkout2.php'
 
 req1 = requests.post(url1, data=data1)
-req2 = requests.post(url2, data=data2)              
+req2 = requests.post(url2, data=data2)  
+
+# in certain condition, you may need to login before you can post the data.
+# to do this, you can use requests.session() to create persistent session after login action.
+# see http://docs.python-requests.org/en/latest/user/advanced/#session-objects
