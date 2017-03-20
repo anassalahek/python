f62a6536019c7f27647e3aa612e5eb1f2a20ebf5
anassalahek <anassalahek@gmail.com>
2017-03-20 21:23:12 +0700

    Update form.py

--- form.py
+++ form.py
@@ -2,5 +2,11 @@
 #!/usr/bin/python
 
 import requests
-import time
-req = requests.post(url, data={'name': Robert'}, auth=(USER, PASS))
+
+data1 = {'name': 'Robert', 'city': 'Oregon'}
+data2 = {'method' : 'creditcard', 'total': 100}
+url1 = 'http://yourserver/checkout1.php'
+url2 = 'http://yourserver/checkout2.php'
+
+req1 = requests.post(url1, data=data1)
+req2 = requests.post(url2, data=data2)              
